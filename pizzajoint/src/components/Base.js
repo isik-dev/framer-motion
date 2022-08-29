4310de8aa6222c8059e6df419dd2915f1cda878f
isik <isik@finset.io>
2022-08-29 21:37:26 +0900

    [basics] initial, animate, transition

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -1,34 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
-    <div className="base container">
-
+    <motion.div
+      initial={{ x: '100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', delay: 0.5 }}
+      className="base container"
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
+        {bases.map((base) => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
-            </li>
-          )
+            <motion.li
+              whileHover={{
+                color: '#f8e112',
+                scale: 1.3,
+                originX: 0,
+              }}
+              transition={{ type: 'spring', stiffness: 300 }}
+              key={base}
+              onClick={() => addBase(base)}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div
+          initial={{ x: '-100vh' }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 120 }}
+          className="next"
+        >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: '0px 0px 8px rgb(255, 255, 255)',
+                boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+              }}
+            >
+              Next
+            </motion.button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Base;
--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ type: 'tween', delay: 1.5, duration: 1.5 }}
+      className="home container"
+    >
+      <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: '0px 0px 8px rgb(255, 255, 255)',
+            boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+          }}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
