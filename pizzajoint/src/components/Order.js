0bc16b4506f2a65a4b8f756901afc5b4ff220f50
isik <isik@finset.io>
2022-08-29 22:10:13 +0900

    [common] transition orchestration: when

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+      when: 'beforeChildren',
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const childrenVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="container order"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      <motion.p variants={childrenVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      {pizza.toppings.map((topping) => (
+        <motion.div variants={childrenVariants} key={topping}>
+          {topping}
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+};
 
 export default Order;
