10544b58979d99f99233fec5a65d455c7faaf5e3
isik <isik@finset.io>
2022-08-30 09:04:20 +0900

    [common] keyframes for infinite animations

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -24,6 +24,18 @@ const nextVariants = {
   },
 };
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255, 255, 255)',
+    boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
@@ -58,13 +70,7 @@ const Base = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div variants={nextVariants} className="next">
           <Link to="/toppings">
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: '0px 0px 8px rgb(255, 255, 255)',
-                boxShadow: '0px 0px 8px rgb(255, 255, 255)',
-              }}
-            >
+            <motion.button variants={buttonVariants} whileHover="hover">
               Next
             </motion.button>
           </Link>
